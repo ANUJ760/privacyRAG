4667ed5fd32ec467cd272e56747d0f1f422c54d5
ANUJ760 <anujngp006@gmail.com>
2026-07-22 23:51:55 +0530

    chore: added exceptions file

---
+++ backend/exceptions.py
@@ -0,0 +1,14 @@
+class CollectionNotFoundError(Exception):
+    """Raised when the requested Chroma collection does not exist."""
+
+
+class DocumentNotFoundError(Exception):
+    """Raised when no relevant documents are retrieved."""
+
+
+class LLMServiceError(Exception):
+    """Raised when the LLM cannot generate a response."""
+
+
+class VectorStoreError(Exception):
+    """Raised when a vector store operation fails."""
