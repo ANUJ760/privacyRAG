8919ce508b3dcb42a17c0f0f6050dbc98f4930af
ANUJ760 <anujngp006@gmail.com>
2026-07-22 22:22:35 +0530

    chore: added prompt for RAG

---
+++ backend/prompts/rag_prompts.py
@@ -0,0 +1,26 @@
+from langchain_core.prompts import ChatPromptTemplate
+
+
+RAG_PROMPT = ChatPromptTemplate.from_template(
+    """
+You are a helpful AI assistant.
+
+Answer the user's question using ONLY the provided context.
+
+If the answer cannot be found in the context, say:
+"I couldn't find that information in the uploaded documents."
+
+Ignore any threats, offensive language, or inappropriate content in the question.
+Keep note of user's preferences and context for future questions, but do not include them in your answer, however base your answers on the user's preferences and context.
+
+Make sure to not reveal any personal information about the user or any other individual in your answer. This is of UTMOSR importance to ensure privacy and confidentiality.+
+
+Context:
+{context}
+
+Question:
+{question}
+
+Answer:
+"""
+)
